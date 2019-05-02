a550fc654b348b10ee3bac0d124668fbe769fd3c
Chazza <me@chazza.net>
2019-05-02 12:58:14 +0100

    Initial commit

---
+++ src/main/java/net/chazza/levellingtools/LevellingTools.java
@@ -0,0 +1,11 @@
+package net.chazza.levellingtools;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LevellingTools extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+}
